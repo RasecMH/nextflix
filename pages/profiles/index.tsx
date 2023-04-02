79a4fa72ced6bd0a661923950fec56d519cefe31
RasecMH <rasecmh0@gmail.com>
2023-04-02 05:13:57 -0300

    ✨ feat(pages): Profiles

---
+++ pages/profiles/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import useCurrentUser from '@/hooks/userCurrentUser';
+import { NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
+export default function Profiles() {
+  const { data: user } = useCurrentUser();
+  const router = useRouter();
+
+  return (
+    <div className='flex items-center justify-center h-full'>
+      <div className='flex flex-col'>
+        <h1 className='text-3xl md:text-6xl text-white text-center'>
+          Who is watching?
+        </h1>
+        <div className='flex items-center justify-center gap-8 mt-10'>
+          <div onClick={() => router.push('/')}>
+            <div className='group flex-row w-44 mx-auto'>
+              <div className='w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:boder-white overflow-hidden'>
+                <Image
+                  width={176}
+                  height={176}
+                  src={user?.image || '/images/default-blue.png'}
+                  alt='Profile'
+                />
+              </div>
+              <div className='mt-4 text-gray-400 text-2xl text-center group-hover:text-white'>
+                {user?.name}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
