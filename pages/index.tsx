7f28bd04e1797c6b547e78fae5d8938bfc53000c
RasecMH <rasecmh0@gmail.com>
2023-04-05 23:40:13 -0300

    ✨ feat(pages): add components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,10 @@
+import Billboard from '@/components/Billboard';
+import InfoModal from '@/components/InfoModal';
+import MovieList from '@/components/MovieList';
 import NavBar from '@/components/NavBar';
+import useFavorites from '@/hooks/useFavorites';
+import useInfoModal from '@/hooks/useInfoModal';
+import useMovieList from '@/hooks/useMovieList';
 import useCurrentUser from '@/hooks/userCurrentUser';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
@@ -21,11 +27,19 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <NavBar />
+      <Billboard />
+      <div className='pb-40'>
+        <MovieList title='Trending Now' data={movies} />
+        <MovieList title='My list' data={favorites} />
+      </div>
     </>
   );
 }
